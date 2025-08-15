e5316ef9348c1f2c286085b472ed91f6d3f57b9c
professor434 <81698289+professor434@users.noreply.github.com>
2025-08-15 17:57:11 +0300

    Add files via upload

---
+++ env (2).ts
@@ -0,0 +1,10 @@
+# --- SOLANA ---
+VITE_SOLANA_RPC=https://broken-purple-breeze.solana-mainnet.quiknode.pro/b087363c02a61ba4c37f9acd5c3c4dcc7b20420f
+VITE_SOLANA_COMMITMENT=confirmed
+VITE_HABIBI_DECIMALS=6
+
+# Θα συμπληρωθεί μετά το script createMint
+VITE_HABIBI_MINT=
+
+# Πραγματικό treasury για τα app-level fees (SPL HABIBI)
+VITE_HABIBI_TREASURY=2WPrEmPFTWfG9WfMDd3W6SGYUvFeTgLbNpAwbJ5nvX4c
